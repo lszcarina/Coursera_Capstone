7906ad460fc80803cab0c5c9080745102126433a
lszcarina <67868551+lszcarina@users.noreply.github.com>
2020-07-22 22:49:11 -0400

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# for capstone project only
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
